ebf78e34aba3a14c62e0e3da9227e915ae9746b7
solomonsitotaw23 <solomonsitotaw9@gmail.com>
2022-08-24 15:42:35 +0300

    Bubbles Landing Page

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+/*
+let anim_canva;
+
+window.onload = function () {
+
+  console.log("load");
+  anim_canva = document.getElementById("be_there");
+  resizeCanvas();
+};
+
+window.onresize = function () {
+  console.log("resize");
+  resizeCanvas();
+
+};
+
+
+function resizeCanvas() {
+  anim_canva.width = window.innerWidth;
+  anim_canva.height = window.innerHeight;
+}
+*/
